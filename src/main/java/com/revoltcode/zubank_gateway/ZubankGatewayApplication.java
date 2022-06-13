62785e310feac35ffc881215d00ba52b0b422fb2
oddy-bassey <oddybassey@gmail.com>
2022-06-13 13:22:44 +0100

    initial project setup

---
+++ src/main/java/com/revoltcode/zubank_gateway/ZubankGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.revoltcode.zubank_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZubankGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZubankGatewayApplication.class, args);
+	}
+
+}
